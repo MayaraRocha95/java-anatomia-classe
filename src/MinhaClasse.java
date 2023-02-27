e331800b603544ec462cdd952b396c6dd25df178
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-02-27 15:49:20 -0300

    anatomia

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,6 +1,17 @@
 public class MinhaClasse {
 
     public static void main (String [] args){
-        System.out.print ("Hello Word");
+    /*  System.out.print ("Hello Word");  */
+        int ano = 2021;
+
+        ano = 2022;
+    /*a variavel ano pode ser modificada  */
+
+
+
+        final String BR = "Brasil";
+
+       /* BR = "Brazil"; o final é colocado somente quando um valor não pode ser mudado   */
+
     }
 }
