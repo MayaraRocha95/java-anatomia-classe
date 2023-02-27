efb2f2f5a1993a63981bcb3b6408ea555d12ef91
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-02-27 16:06:45 -0300

    anatomia de metodos

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,12 +1,22 @@
 public class MinhaClasse {
 
     public static void main (String [] args){
-        String meuNome = "Mayara";
+        String primeiroNome = "Mayara";
+        String segundoNome = "Rocha";
 
         int anoFabricacao = 2023;
 
         boolean verdadeira = false;
 
         anoFabricacao = 2022;
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        System.out.print(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome ){
+        return "Resultado do Método igual a " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
+
+
