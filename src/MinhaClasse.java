8d99fb9d547b112d3b59ce92fa200ed68fef1816
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2023-02-27 15:52:28 -0300

    metodos

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,17 +1,12 @@
 public class MinhaClasse {
 
     public static void main (String [] args){
-    /*  System.out.print ("Hello Word");  */
-        int ano = 2021;
+        String meuNome = "Mayara";
 
-        ano = 2022;
-    /*a variavel ano pode ser modificada  */
+        int anoFabricacao = 2023;
 
+        boolean verdadeira = false;
 
-
-        final String BR = "Brasil";
-
-       /* BR = "Brazil"; o final é colocado somente quando um valor não pode ser mudado   */
-
+        anoFabricacao = 2022;
     }
 }
